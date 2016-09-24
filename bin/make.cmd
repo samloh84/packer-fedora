fa4d5d6e0022f3f8d271753482cca7a9a5e4848e
Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>
2016-09-25 07:33:19 +0800

    Fixing tty-less sudo Replacing old credentials with vagrant credentials

--- bin/make.cmd
+++ bin/make.cmd
@@ -1,10 +1,8 @@
 REM To force build, add -force to arguments
-
+REM To build for only one builder, add -only=virtualbox-iso or -only=vmware-iso
 pushd ..
-packer.exe build -var-file=centos-7-workstation.json %* centos.json
-packer.exe build -var-file=centos-7-server.json %* centos.json
-
-ovftool.exe output\centos-7-server\vmware-iso\centos-7-server.vmx output\centos-7-server.vmware.ova
-ovftool.exe output\centos-7-workstation\vmware-iso\centos-7-workstation.vmx output\centos-7-workstation.vmware.ova
-
+packer.exe build -var-file=fedora-24-server.json %* fedora.json
+packer.exe build -var-file=fedora-24-workstation.json %* fedora.json
+packer.exe build -var-file=fedora-23-server.json %* fedora.json
+packer.exe build -var-file=fedora-23-workstation.json %* fedora.json
 popd
