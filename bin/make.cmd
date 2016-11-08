765f39d4fa8915e3cdf30b5c09b8c349de1efa7b
Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>
2016-11-08 21:15:46 +0800

    Refactoring windows build script again.

--- bin/make.cmd
+++ bin/make.cmd
@@ -3,56 +3,79 @@
 REM To force build, add -force to arguments
 REM To build for only one builder, add -only=virtualbox-iso or -only=vmware-iso
 
-SET FEDORA24SERVER=0
-SET FEDORA24WORKSTATION=0
-SET FEDORA23SERVER=0
-SET FEDORA23WORKSTATION=0
+SET NO_OPTS=1
 
-echo Building %1
-IF [%1] == [] SET FEDORA24SERVER=1
-IF [%1] == [] SET FEDORA24WORKSTATION=1
-IF [%1] == [] SET FEDORA23SERVER=1
-IF [%1] == [] SET FEDORA23WORKSTATION=1
+SET FEDORA_24_SERVER=0
+SET FEDORA_24_WORKSTATION=0
+SET FEDORA_23_SERVER=0
+SET FEDORA_23_WORKSTATION=0
 
-IF "%1" == "all" SET FEDORA24SERVER=1
-IF "%1" == "all" SET FEDORA24WORKSTATION=1
-IF "%1" == "all" SET FEDORA23SERVER=1
-IF "%1" == "all" SET FEDORA23WORKSTATION=1
+SET REST_ARGS=
 
-IF "%1" == "fedora-24" SET FEDORA24SERVER=1
-IF "%1" == "fedora-24" SET FEDORA24WORKSTATION=1
-IF "%1" == "fedora-23" SET FEDORA23SERVER=1
-IF "%1" == "fedora-23" SET FEDORA23WORKSTATION=1
+:GETOPTS
+SET OPTS_MATCHED=0
+IF "%1" == "all" (
+	SET FEDORA_24_SERVER=1
+	SET FEDORA_24_WORKSTATION=1
+	SET FEDORA_23_SERVER=1
+	SET FEDORA_23_WORKSTATION=1
+)
 
-IF "%1" == "fedora-24-server" SET FEDORA24SERVER=1
-IF "%1" == "fedora-24-workstation" SET FEDORA24WORKSTATION=1
-IF "%1" == "fedora-23-server" SET FEDORA23SERVER=1
-IF "%1" == "fedora-23-workstation" SET FEDORA23WORKSTATION=1
+IF "%1" == "fedora-24" (
+	SET FEDORA_24_SERVER=1
+	SET FEDORA_24_WORKSTATION=1
+)
 
-set RESTVAR=
-shift
-:loop1
-if "%1"=="" goto after_loop
-set RESTVAR=%RESTVAR% %1
-shift
-goto loop1
+IF "%1" == "fedora-23" (
+	SET FEDORA_23_SERVER=1
+	SET FEDORA_23_WORKSTATION=1
+)
 
-:after_loop
+IF "%1" == "fedora-24-server" (
+	SET FEDORA_24_SERVER=1
+)
+
+IF "%1" == "fedora-24-workstation" (
+	SET FEDORA_24_WORKSTATION=1
+)
+
+IF "%1" == "fedora-23-server" (
+	SET FEDORA_23_SERVER=1
+)
+
+IF "%1" == "fedora-23-workstation" (
+	SET FEDORA_23_WORKSTATION=1
+)
+
+SHIFT
+IF OPTS_MATCHED EQU 0 SET REST_ARGS=%REST_ARGS% %1
+IF NOT "%1" == "" GOTO GETOPTS
+
+IF NO_OPTS EQU 1 (
+	SET FEDORA_24_SERVER=1
+    SET FEDORA_24_WORKSTATION=1
+    SET FEDORA_23_SERVER=1
+    SET FEDORA_23_WORKSTATION=1
+)
+
+
 pushd ..
-IF DEFINED FEDORA24SERVER (
-packer.exe build -var-file=fedora-24-server.json %RESTVAR% fedora.json
-ovftool.exe --overwrite output\vmware-iso\fedora-24-server\fedora-24-server.vmx output\fedora-24-server.ova
+IF FEDORA_24_SERVER EQU 1 (
+	packer.exe build -var-file=fedora-24-server.json %REST_ARGS% fedora.json
+	ovftool.exe --overwrite output\vmware-iso\fedora-24-server\fedora-24-server.vmx output\fedora-24-server.ova
 )
-IF DEFINED FEDORA24WORKSTATION (
-packer.exe build -var-file=fedora-24-workstation.json %RESTVAR% fedora.json
-ovftool.exe --overwrite output\vmware-iso\fedora-24-workstation\fedora-24-workstation.vmx output\fedora-24-workstation.ova
+IF FEDORA_24_WORKSTATION EQU 1 (
+	packer.exe build -var-file=fedora-24-workstation.json %REST_ARGS% fedora.json
+	ovftool.exe --overwrite output\vmware-iso\fedora-24-workstation\fedora-24-workstation.vmx output\fedora-24-workstation.ova
 )
-IF DEFINED FEDORA23SERVER (
-packer.exe build -var-file=fedora-23-server.json %RESTVAR% fedora.json
-ovftool.exe --overwrite output\vmware-iso\fedora-23-server\fedora-24-server.vmx output\fedora-24-server.ova
+
+IF FEDORA_23_SERVER EQU 1 (
+	packer.exe build -var-file=fedora-23-server.json %REST_ARGS% fedora.json
+	ovftool.exe --overwrite output\vmware-iso\fedora-23-server\fedora-23-server.vmx output\fedora-23-server.ova
 )
-IF DEFINED FEDORA23WORKSTATION (
-packer.exe build -var-file=fedora-23-workstation.json %RESTVAR% fedora.json
-ovftool.exe --overwrite output\vmware-iso\fedora-23-workstation\fedora-23-workstation.vmx output\fedora-23-workstation.ova
+IF FEDORA_23_WORKSTATION EQU 1 (
+	packer.exe build -var-file=fedora-23-workstation.json %REST_ARGS% fedora.json
+	ovftool.exe --overwrite output\vmware-iso\fedora-23-workstation\fedora-23-workstation.vmx output\fedora-23-workstation.ova
 )
+
 popd
