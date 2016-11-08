4b3d150bec03a30c40db01014e0f332d35b46e7e
Samuel Loh <samuel_loh@ida.gov.sg>
2016-11-08 15:39:34 +0800

    Revised all the kickstart files to have options compatible with the respective Fedora versions Revised windows build scripts

--- bin/make.cmd
+++ bin/make.cmd
@@ -1,4 +1,5 @@
 @echo off
+
 REM To force build, add -force to arguments
 REM To build for only one builder, add -only=virtualbox-iso or -only=vmware-iso
 
@@ -31,16 +32,27 @@ IF "%1" == "fedora-23-workstation" SET FEDORA23WORKSTATION=1
 set RESTVAR=
 shift
 :loop1
-if [%1] == [] goto after_loop
+if "%1"=="" goto after_loop
 set RESTVAR=%RESTVAR% %1
 shift
 goto loop1
 
 :after_loop
-
 pushd ..
-IF %FEDORA24SERVER% EQU 1 packer.exe build -var-file=fedora-24-server.json %RESTVAR% fedora.json
-IF %FEDORA24WORKSTATION% EQU 1 packer.exe build -var-file=fedora-24-workstation.json %RESTVAR% fedora.json
-IF %FEDORA23SERVER% EQU 1 packer.exe build -var-file=fedora-23-server.json %RESTVAR% fedora.json
-IF %FEDORA23WORKSTATION% EQU 1 packer.exe build -var-file=fedora-23-workstation.json %RESTVAR% fedora.json
+IF DEFINED FEDORA24SERVER (
+packer.exe build -var-file=fedora-24-server.json %RESTVAR% fedora.json
+ovftool.exe --overwrite output\vmware-iso\fedora-24-server\fedora-24-server.vmx output\fedora-24-server.ova
+)
+IF DEFINED FEDORA24WORKSTATION (
+packer.exe build -var-file=fedora-24-workstation.json %RESTVAR% fedora.json
+ovftool.exe --overwrite output\vmware-iso\fedora-24-workstation\fedora-24-workstation.vmx output\fedora-24-workstation.ova
+)
+IF DEFINED FEDORA23SERVER (
+packer.exe build -var-file=fedora-23-server.json %RESTVAR% fedora.json
+ovftool.exe --overwrite output\vmware-iso\fedora-23-server\fedora-24-server.vmx output\fedora-24-server.ova
+)
+IF DEFINED FEDORA23WORKSTATION (
+packer.exe build -var-file=fedora-23-workstation.json %RESTVAR% fedora.json
+ovftool.exe --overwrite output\vmware-iso\fedora-23-workstation\fedora-23-workstation.vmx output\fedora-23-workstation.ova
+)
 popd
