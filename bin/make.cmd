0a052aa2dea946eb1e233142c4a33d32ea1029b2
Samuel Loh <samuel_loh@ida.gov.sg>
2016-11-21 13:45:22 +0800

    Fixing output filenames and directories

--- bin/make.cmd
+++ bin/make.cmd
@@ -7,8 +7,10 @@ SET NO_OPTS=1
 
 SET FEDORA_24_SERVER=0
 SET FEDORA_24_WORKSTATION=0
+SET FEDORA_24_DEVELOPER_WORKSTATION=0
 SET FEDORA_23_SERVER=0
 SET FEDORA_23_WORKSTATION=0
+SET FEDORA_23_DEVELOPER_WORKSTATION=0
 SET ONLY_VMWARE_ISO=0
 SET ONLY_VIRTUALBOX_ISO=0
 
@@ -24,20 +26,24 @@ SET OPTS_MATCHED=0
 IF /I "%~1" == "all" (
 	SET FEDORA_24_SERVER=1
 	SET FEDORA_24_WORKSTATION=1
+	SET FEDORA_24_DEVELOPER_WORKSTATION=1
 	SET FEDORA_23_SERVER=1
 	SET FEDORA_23_WORKSTATION=1
+	SET FEDORA_23_DEVELOPER_WORKSTATION=1
 	SET OPTS_MATCHED=1
 )
 
 IF /I "%~1" == "fedora-24" (
 	SET FEDORA_24_SERVER=1
 	SET FEDORA_24_WORKSTATION=1
+	SET FEDORA_24_DEVELOPER_WORKSTATION=1
 	SET OPTS_MATCHED=1
 )
 
 IF /I "%~1" == "fedora-23" (
 	SET FEDORA_23_SERVER=1
 	SET FEDORA_23_WORKSTATION=1
+	SET FEDORA_23_DEVELOPER_WORKSTATION=1
 	SET OPTS_MATCHED=1
 )
 
@@ -51,6 +57,11 @@ IF /I "%~1" == "fedora-24-workstation" (
 	SET OPTS_MATCHED=1
 )
 
+IF /I "%~1" == "fedora-24-developer-workstation" (
+	SET FEDORA_24_DEVELOPER_WORKSTATION=1
+	SET OPTS_MATCHED=1
+)
+
 IF /I "%~1" == "fedora-23-server" (
 	SET FEDORA_23_SERVER=1
 	SET OPTS_MATCHED=1
@@ -58,6 +69,11 @@ IF /I "%~1" == "fedora-23-server" (
 
 IF /I "%~1" == "fedora-23-workstation" (
 	SET FEDORA_23_WORKSTATION=1
+	SET OPTS_MATCHED=1
+)
+
+IF /I "%~1" == "fedora-23-developer-workstation" (
+	SET FEDORA_23_DEVELOPER_WORKSTATION=1
 	SET OPTS_MATCHED=1
 )
 
@@ -86,8 +102,10 @@ IF /I NOT "%~1" == "" GOTO GETOPTS
 IF %NO_OPTS% EQU 1 (
 	SET FEDORA_24_SERVER=1
     SET FEDORA_24_WORKSTATION=1
+    SET FEDORA_24_DEVELOPER_WORKSTATION=1
     SET FEDORA_23_SERVER=1
     SET FEDORA_23_WORKSTATION=1
+    SET FEDORA_23_DEVELOPER_WORKSTATION=1
 )
 
 IF %ONLY_VMWARE_ISO% EQU 1 (
@@ -104,9 +122,11 @@ echo REST_ARGS=%BUILDER_ARGS% %ON_ERROR_ARGS%
 
 echo FEDORA_24_SERVER=%FEDORA_24_SERVER%
 echo FEDORA_24_WORKSTATION=%FEDORA_24_WORKSTATION%
+echo FEDORA_24_DEVELOPER_WORKSTATION=%FEDORA_24_DEVELOPER_WORKSTATION%
 
 echo FEDORA_23_SERVER=%FEDORA_23_SERVER%
 echo FEDORA_23_WORKSTATION=%FEDORA_23_WORKSTATION%
+echo FEDORA_23_DEVELOPER_WORKSTATION=%FEDORA_23_DEVELOPER_WORKSTATION%
 
 pushd ..
 IF %FEDORA_24_SERVER% EQU 1 (
@@ -118,6 +138,10 @@ IF %FEDORA_24_WORKSTATION% EQU 1 (
 	packer.exe build -force -var-file=fedora-24-workstation.json %BUILDER_ARGS% %ON_ERROR_ARGS% fedora.json
 	ovftool.exe --overwrite output\vmware-iso\fedora-24-workstation\fedora-24-workstation.vmx output\fedora-24-workstation.ova
 )
+IF %FEDORA_24_DEVELOPER_WORKSTATION% EQU 1 (
+	packer.exe build -force -var-file=fedora-24-workstation.json %BUILDER_ARGS% %ON_ERROR_ARGS% fedora-developer.json
+	ovftool.exe --overwrite output\vmware-iso\fedora-24-developer-workstation\fedora-24-developer-workstation.vmx output\fedora-24-developer-workstation.ova
+)
 
 IF %FEDORA_23_SERVER% EQU 1 (
 	packer.exe build -force -var-file=fedora-23-server.json %BUILDER_ARGS% %ON_ERROR_ARGS% fedora.json
@@ -127,5 +151,10 @@ IF %FEDORA_23_WORKSTATION% EQU 1 (
 	packer.exe build -force -var-file=fedora-23-workstation.json %BUILDER_ARGS% %ON_ERROR_ARGS% fedora.json
 	ovftool.exe --overwrite output\vmware-iso\fedora-23-workstation\fedora-23-workstation.vmx output\fedora-23-workstation.ova
 )
+IF %FEDORA_23_DEVELOPER_WORKSTATION% EQU 1 (
+	packer.exe build -force -var-file=fedora-23-workstation.json %BUILDER_ARGS% %ON_ERROR_ARGS% fedora-developer.json
+	ovftool.exe --overwrite output\vmware-iso\fedora-23-developer-workstation\fedora-23-developer-workstation.vmx output\fedora-23-developer-workstation.ova
+)
+
 
 popd
